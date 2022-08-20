20d39ddabb61f1606c362644a9ddcce6fe3d4715
yts38n <nyt9338@gmail.com>
2022-08-21 06:07:50 +0800

    Update 2022-08-20T22:07:43.459Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,49 +1,23 @@
 "use strict";
 
-var data = [{
-  "id": 0,
-  "name": "BJ41600S",
-  "imgUrl": "./assets/images/product-1.png",
-  "price": 3490,
-  "colors": ["#5F3E2D", "#B75929"]
-}, {
-  "id": 1,
-  "name": "BJ41600S",
-  "imgUrl": "./assets/images/product-2.png",
-  "price": 3490,
-  "colors": ["#5F3E2D", "#B75929"]
-}, {
-  "id": 2,
-  "name": "BJ41600S",
-  "imgUrl": "./assets/images/product-3.png",
-  "price": 3490,
-  "colors": ["#5F3E2D", "#B75929"]
-}, {
-  "id": 3,
-  "name": "BJ41600S",
-  "imgUrl": "./assets/images/product-4.png",
-  "price": 3490,
-  "colors": ["#5F3E2D", "#B75929"]
-}, {
-  "id": 4,
-  "name": "BJ41600S",
-  "imgUrl": "./assets/images/product-5.png",
-  "price": 3490,
-  "colors": ["#5F3E2D", "#B75929"]
-}, {
-  "id": 5,
-  "name": "BJ41600S",
-  "imgUrl": "./assets/images/product-6.png",
-  "price": 3490,
-  "colors": ["#5F3E2D", "#B75929"]
-}]; //執行預設頁面載入
+console.log('Hello!');
+$(document).ready(function () {
+  console.log('HesSchool Hello!');
+});
+"use strict";
 
-init(); //預設頁面載入
+var path = window.location.pathname;
+var data = [];
+var productList = document.querySelector('.productList');
+
+if (path.startsWith("/product") && path.endsWith(".html")) {
+  init();
+}
 
 function init() {
   axios.get("https://raw.githubusercontent.com/yts38n/webLayoutTr2002_week4/main/app/json/product-optical.json").then(function (res) {
     data = res.data.data;
-    console.log(data);
+    renderHTML(data);
   })["catch"](function (error) {
     console.log(error);
   });
@@ -54,8 +28,12 @@ function init() {
 function renderHTML(ary) {
   var str = "";
   ary.forEach(function (item) {
-    str += "\n        <li class=\"productCard\">\n          \t<img src=".concat(item["imgUrl"], " alt=\"").concat(item["name"], "\">\n            <div class=\"productCard-content\">\n                <div>\n                    <h3>").concat(item["name"], "</h3>\n                    <p>").concat(item["price"], "</p>\n                </div>\n                <div>\n\t\t\t\t\t").concat(item["colors"], "\n                </div>\n            </div>\n        </li>");
-    productCard.innerHTML = str;
+    var colors = "";
+    item["colors"].forEach(function (item) {
+      colors += "<div class=\"paletteCard-".concat(item, "\"></div>");
+    });
+    str += "\n            <li class=\"productCard\">\n                <img src=\"".concat(item["imgUrl"], "\" alt=\"").concat(item["name"], "\">\n                <div class=\"productCard-content\">\n                    <h3>").concat(item["name"], "</h3>\n                    <p>").concat(item["price"], "</p>\n                    <div class=\"palette\">\n                        ").concat(colors, "\n                    </div>\n                </div>\n            </li>");
+    productList.innerHTML = str;
   });
 }
 
